ed9ada307ba42fc177230ca460f15009928266c9
dieme5 <elhadjimamadoudieme5@gmail.com>
2024-06-30 10:28:09 +0000

    Observable avec un observeur subscribe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-import { AppareilService } from './services/appareil.service';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+
+  secondes!: number;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    const counter = interval(1000);
+    counter.subscribe({
+      next: (value: number) => {
+        this.secondes = value;
+      },
+      error: (err) => {
+        console.log('Une erreur a été rencontrée !' + err);
+      },
+      complete: () => {
+        console.log('Observable complétée !');
+      },
+    });
+  }
+}
